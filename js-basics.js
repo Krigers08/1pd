46aa5a58f7baff3900e0997cd5f7ee517bcc2db5
elinaviksne <elina@gmail.com>
2024-11-20 15:46:21 +0200

    .\index.html

---
+++ js-basics.js
@@ -0,0 +1,54 @@
+// let firstName = "Jānis";
+// console.log("Kristes");
+
+// let myArray = [1,2,3,4];
+// let x = 11;
+// if (x == 10){
+//     console.log("x ir vienāds ar 11")
+// }
+
+// let person = {
+//     vards: "Anna",
+//     irStudents: true,
+// }
+// console.log(person.irStudents); //dot notaion
+// console.log(person["vards"]);
+
+// let attribute = "vards";
+// console.log(person[attribute]);
+
+// console.log(person.vecums);
+// person.vecums = 16;
+// console.log(person.vecums);
+
+// //termary operator
+// let ageStatus = (person.vecums >=18) ? "Pilngadīgs" : " Nepilngadīgs";
+// console.log(ageStatus);
+
+
+// for(let i = 1 ; i < 5; i++){
+//     console.log(i)
+// }
+
+// let namesArray = ["Līga", "Anna", "Antra"]
+// namesArray[2];
+
+// for (let i = 0; i <= namesArray.length; i++){
+//     console.log(namesArray[i]);
+// }
+
+
+function getRectArea() {
+    return 3 * 4
+}
+
+let result = getRectArea();
+console.log(result);
+
+
+function getRectAreaAdvanced(width, height) {
+    return width * height;
+}
+
+console.log(getRectAreaAdvanced);
+
